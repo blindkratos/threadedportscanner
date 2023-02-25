ebcdf51afd80fcfeeec3d2be646b25c0366190a2
blindkratos <110800807+blindkratos@users.noreply.github.com>
2023-02-25 17:26:06 +1000

    Added ability to modify total thread count
    I added an additional argument to the command line to allow max threads to be defined when running the scan. If left blank, it will run with the max available to ThreadPoolExecutor being defined by the length of the list of ports (total ports to be scanned equals max available threads), however now if '-t' is defined as 10, the max available argument for the ThreadPoolExecutor function will be limited to 10 (or any number chosen by the user). This allows the user to decide to use less to make a scan less aggressive, or to potentially limit a spike in memory usage if using this program on a machine with limited resources.

--- portScanner.py
+++ portScanner.py
@@ -15,9 +15,9 @@ def check_ports(host, port):
         return False # returns false if connection is unsuccessful
         
 # scan port numbers on a host
-def scan_ports(host, ports):
-    print(f'Scanning IP: {host} , Ports: {ports[0]} to {ports[-1]}')
-    with ThreadPoolExecutor(len(ports)) as executor: # assigning ThreadPoolExecutor with argument length of list of ports as max_workers to variable name 'executor'
+def scan_ports(host, ports, threads):
+    print(f'Scanning IP: {host} , Ports: {ports[0]} to {ports[-1]} with {threads} threads...')
+    with ThreadPoolExecutor(threads) as executor: # assigning ThreadPoolExecutor with argument length of list of ports as max_workers to variable name 'executor'
         results = executor.map(check_ports, [host]*len(ports), ports) 
         # Calling the executor.map function with arguments check_ports function, a list of hosts multiplied by the length of the ports list, and the ports list. 
         # This concurrently runs the check_ports function against the lists of hosts and ports using threads allocated by ThreadPoolExecutor.
@@ -33,16 +33,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A multi-threaded port scanner written in python', formatter_class=argparse.RawDescriptionHelpFormatter, epilog='Example: portScanner.py 127.0.0.1 -p 1-1024')
     parser.add_argument('target', help='The target IP address to be scanned')
     parser.add_argument('-p', '--ports', help='The range of ports to be scanned, separated by \'-\', or a list of ports separated by \',\'')
+    parser.add_argument('-t', '--threads', type=int, help='The max number of threads to use. Default is max available to ThreadPoolExecutor')
     args = parser.parse_args() # command line arguments assigned and defined above
     target = args.target
+    
     if args.ports != None and '-' in args.ports: # if there is a dash in the ports value, change the port values to a list including all ports between minimum and maximum port values plus one
         [min_p, max_p] = [int(i) for i in args.ports.split('-')]
         ports = range(min_p, max_p + 1)
     elif args.ports != None: # if the ports argument does not contain a dash, change port values to a list of defined port values separated by a comma
         ports = [int(i) for i in args.ports.split(',')]
         
+    if args.threads == None:
+        threads = len(ports)
+    else:
+        threads = args.threads
+    
     try:
-        scan_ports(target, ports)
+        scan_ports(target, ports, threads)
     except KeyboardInterrupt: # handle ctrl-c
         print('Scan cancelled by user!')
         sys.exit()
